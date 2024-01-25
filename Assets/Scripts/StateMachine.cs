48e134dd135ed570fc510f14ec4ee127ce5ae383
pduren <57735971+kParkz@users.noreply.github.com>
2024-01-25 12:55:07 -0600

    trace logic begin to end + fail

--- Assets/Scripts/StateMachine.cs
+++ Assets/Scripts/StateMachine.cs
@@ -1,3 +1,4 @@
 public static class SM {
     public static int displayTextIterator = 0;
+    public static bool startObjTraceFlag = false;
 }
--- Assets/Scripts/Trace.cs
+++ Assets/Scripts/Trace.cs
@@ -17,9 +17,18 @@ public class Trace : MonoBehaviour
         var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
 
-        if (Physics.Raycast(ray, out hit) && hit.transform.tag == "traceObject") {
+        
+        if (Physics.Raycast(ray, out hit) && hit.transform.tag == "objBegin") {
+            Debug.Log("begin trace");
+            SM.startObjTraceFlag = true;
+        } else if (Physics.Raycast(ray, out hit) && hit.transform.tag == "objTrace" && SM.startObjTraceFlag) {
             Destroy(hit.transform.gameObject);
             breakSound.Play(0);
+        } else if (Physics.Raycast(ray, out hit) && hit.transform.tag == "objEnd") {
+            Debug.Log("end trace");
+            SM.startObjTraceFlag = false;
+        } else if (Physics.Raycast(ray, out hit) && !hit.transform.tag.Contains("obj") && SM.startObjTraceFlag) {
+            Debug.Log("fail!");
         }
     }
 }
