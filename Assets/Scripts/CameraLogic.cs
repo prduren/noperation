d5e0c3ba67d4f0033c871874f986faf3db124763
pduren <57735971+kParkz@users.noreply.github.com>
2024-03-09 18:21:39 -0600

    camera logic

--- Assets/Scripts/CameraLogic.cs
+++ Assets/Scripts/CameraLogic.cs
@@ -10,7 +10,16 @@ public class CameraLogic : MonoBehaviour {
     private float initFieldOfView;
     public float duration = 1;
     private bool initFOVReturnFlag = false;
-    
+    public Vector3 paintingCameraLoc;
+    public Quaternion paintingCameraRot;
+    public Vector3 kitchenCameraLoc;
+    public Quaternion kitchenCameraRot;
+    public Vector3 windowCameraLoc;
+    public Quaternion windowCameraRot;
+    public Vector3 tvCameraLoc;
+    public Quaternion tvCameraRot;
+    public Vector3 doorCameraLoc;
+    public Quaternion doorCameraRot;
 
     // Calculate the frustum height at a given distance from the camera.
     float FrustumHeightAtDistance(float distance) {
@@ -45,13 +54,15 @@ public class CameraLogic : MonoBehaviour {
             // Measure the new distance and readjust the FOV accordingly.
             var currDistance = Vector3.Distance(transform.position, target.position);
             newCamera.fieldOfView = FOVForHeightAndDistance(initHeightAtDist, currDistance);
+            
+            if (newCamera.transform.position.z > -22f && SM.beginGamePostIntro) { // this IF used to be in the Update() root. I moved it to ensure camera position translation does not occur when we move the camera to view other objects.
+                transform.Translate(Vector3.back * Time.deltaTime * 28f);
+            }
         }
         
         // Simple control to allow the camera to be moved in and out using the up/down arrows.
         // transform.Translate(Input.GetAxis("Vertical") * Vector3.forward * Time.deltaTime * 28f);
-        if (newCamera.transform.position.z > -22f && SM.beginGamePostIntro) {
-            transform.Translate(Vector3.back * Time.deltaTime * 28f);
-        }
+        
         if (newCamera.transform.position.z < -21f) {
             dzEnabled = false;
             newCamera.fieldOfView = Mathf.MoveTowards(newCamera.fieldOfView, initFieldOfView, 100f * Time.deltaTime);
@@ -61,9 +72,34 @@ public class CameraLogic : MonoBehaviour {
         }
     }
 
+    public void GoToPainting() {
+        newCamera.transform.position = paintingCameraLoc;
+        newCamera.transform.rotation = paintingCameraRot;
+    }
+
+    public void GoToKitchen() {
+        newCamera.transform.position = kitchenCameraLoc;
+        newCamera.transform.rotation = kitchenCameraRot;
+    }
+
+    public void GoToWindow() {
+        newCamera.transform.position = windowCameraLoc;
+        newCamera.transform.rotation = windowCameraRot;
+    }
+
+    public void GoToTV() {
+        newCamera.transform.position = tvCameraLoc;
+        newCamera.transform.rotation = tvCameraRot;
+    }
+
+    public void GoToDoor() {
+        newCamera.transform.position = doorCameraLoc;
+        newCamera.transform.rotation = doorCameraRot;
+    }
+
     IEnumerator IntroZoomDone() {
         yield return new WaitForSeconds(3);
         SM.introZoomDone = true;
-        Debug.Log("my coroutine is done");
+        StopDZ();
     }
 }
